d7537ee448f964f3a16be5ffc49523ec2ac60118
yasueri <127986902+yasueri@users.noreply.github.com>
2025-04-12 14:21:38 +0900

    Add files via upload

---
+++ data/lt-csv.js
@@ -0,0 +1,17 @@
+window.csvData = `StepID,タイトル,説明１,補足説明１,説明２,説明３,Option1Text,Option1Next,Option2Text,Option2Next,Option3Text,Option3Next,DefaultNext,NonAutoSelect
+1,,,,,,,,,,,,2,
+2,,,,,,今回のテストへ進む,b1,,,,,,
+b1,バグ再現１,<img src="x" onerror="alert('ハッキングされました!')">,,,,,,,,,,b2,
+b2,,,,,,あり,b3,なし,b3,,,,
+b3,,,,,,b,b4,a,b5,,,,
+b4,select b,,,,,,,,,,,b6,
+b5,select a,,,,,,,,,,,b6,
+b6,,,,,,,,,,,,b7,
+b7,,,,,,あり,b8,なし,b8,,,,
+b8,,,,,,b,b9,a,b10,,,,
+b9,bが選ばれた,,,,,,,,,,,b11,
+b10,aが選ばれた,,,,,,,,,,,b11,
+b11,,,,,,,,,,,,b12,
+b12,,,,,,,,,,,,,
+
+`;
